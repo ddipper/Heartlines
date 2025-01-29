fe41f20381fd346855340b979d2d9047ab1c6909
Matissonn <116301445+Matissonn@users.noreply.github.com>
2025-01-29 14:29:33 +0300

    temp

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Tiles/Interactable")]
+
+public class Interactable : Tile
+{
+    [SerializeField] private int dddd;
+    [SerializeField] private int ID;
+
+}
