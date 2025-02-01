76c10330d5db57a3ce0eb6be3a1d86d46f894806
Dmitry <dev.dbuts@gmail.com>
2025-02-01 13:04:11 +0300

    rewrite outline and interaction

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,20 +1,45 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+using UnityEngine.Serialization;
 
-[CreateAssetMenu(menuName = "Tiles/Interactable")]
-
-public class Interactable : Tile
+public class Outline : MonoBehaviour
 {
-    [SerializeField] private int ID;
+    [SerializeField] private Material outlineMat;
+    private Material defaultMat;
+    private SpriteRenderer _spriteRenderer;
 
-    public int GetTileID(Vector3Int position)
+    [SerializeField] private string interactionText;
+    [SerializeField] private Text uiText;
+    
+    void Start()
     {
-        ID = 1000 * position.x + position.y;
-        return ID;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultMat = _spriteRenderer.material;
+        
+        uiText.text = "";
+        uiText.gameObject.SetActive(false);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _spriteRenderer.material = outlineMat;
+
+            uiText.text = interactionText;
+            uiText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _spriteRenderer.material = defaultMat;
+            uiText.text = "";
+            uiText.gameObject.SetActive(false);
+        }
     }
 }
